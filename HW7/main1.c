4a9446fd5235562b62db8bfabcc6d9a50e200709
B1129029 <120006585+n930827@users.noreply.github.com>
2023-01-04 14:14:36 +0800

    Create main1.c

---
+++ HW7/main1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    for (int i=1;i<8;i++){
+        printf("%*s",8-i," ");
+        for(int j=0;j<i;j++){
+            printf("%d ",i);
+        }
+        printf("\n");
+    } 
+    return 0;
+}
